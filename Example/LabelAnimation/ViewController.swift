61261e04400acb629b10f39892ac7bea89cf068c
satheesh <orugantisatheesh@gmail.com>
2020-01-15 19:34:26 +0530

    file added

--- Example/LabelAnimation/ViewController.swift
+++ Example/LabelAnimation/ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import LabelAnimation
+import LabelAnimViewController
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let animation = LabelAnimViewController
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ Example/Pods/Target Support Files/Pods-LabelAnimation_Tests/Pods-LabelAnimation_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LabelAnimation_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LabelAnimation_Tests
+@end
