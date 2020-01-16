3d00336d536b0d791e17c43e87fb064b1d98db9c
satheesh <orugantisatheesh@gmail.com>
2020-01-16 14:20:31 +0530

    pod updated with label var

--- LabelAnimation/Classes/LabelAnimViewController.swift
+++ LabelAnimation/Classes/LabelAnimViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 public class LabelAnimViewController: UIViewController {
 
+    @IBOutlet weak public var animLabel: UILabel!
     override public func viewDidLoad() {
         super.viewDidLoad()
 
